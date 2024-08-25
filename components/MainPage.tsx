565ec1120566d7aa8dee4b89e19a21721a95176e
Merli Mejia <merli@beatsunion.com>
2024-08-25 08:36:18 -0400

    Can Add.

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -1,8 +1,39 @@
-import { Button, Text } from "@rneui/themed";
+import { Button, Input, Text } from "@rneui/themed";
 import { Alert, StyleSheet, View } from "react-native";
 import { supabase } from "../lib/supabase";
+import { useState } from "react";
 
 export const MainPage = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const addItem = async () => {
+    const { data: userResponse } = await supabase.auth.getUser();
+    const user = userResponse.user;
+
+    const now = new Date();
+
+    if (!user) {
+      Alert.alert("You must be signed in to add an item");
+      return;
+    }
+
+    const { data, error } = await supabase.from("items").insert([
+      {
+        title: title,
+        desc: desc,
+        user_id: user.id,
+        updated_at: now.toISOString(),
+      },
+    ]);
+    if (error) Alert.alert(error.message);
+    else {
+      setTitle("");
+      setDesc("");
+      Alert.alert("Item Added Successfully");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={[styles.verticallySpaced, styles.mt20]}>
@@ -14,6 +45,25 @@ export const MainPage = () => {
             if (error) Alert.alert(error.message);
           }}
         />
+      </View>
+      <View
+        style={{
+          alignSelf: "stretch",
+        }}
+      >
+        <Input
+          placeholder="Title"
+          value={title}
+          onChangeText={(e) => setTitle(e)}
+        />
+        <Input
+          placeholder="Description"
+          multiline
+          numberOfLines={10}
+          value={desc}
+          onChangeText={(e) => setDesc(e)}
+        />
+        <Button title="Add" onPress={addItem} />
       </View>
     </View>
   );
@@ -31,12 +81,15 @@ const styles = StyleSheet.create({
     paddingTop: 4,
     paddingBottom: 4,
     alignSelf: "stretch",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
     color: "black",
-    marginBottom: 20,
     textAlign: "center",
   },
   mt20: {
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,14 +1,15 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient } from '@supabase/supabase-js'
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../database.types";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl!, supabaseAnonKey!, {
+export const supabase = createClient<Database>(supabaseUrl!, supabaseAnonKey!, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
   },
-})
+});
